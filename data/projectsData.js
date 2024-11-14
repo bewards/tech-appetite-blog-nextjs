deea04aa590fd25beaa8f73f0afafba08516f523
Ben Sewards <bsewards@rightpoint.com>
2024-11-14 16:05:17 -0500

    client projects update

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,13 @@
 const projectsData = [
   {
+    title: 'UPMC Health Plan',
+    description: `Healthcare Platform innovation team project created with Sitecore Headless CMS Next.js PaaS. Co-Development and mentoring six client developers while maintaining client-facing responsibilities. Activities included a build-once deploy everywhere blue-green CI/CD process, React development through Figma component design, Storybook integration with Sitecore, and Bootstrap v5.1 custom theme.`,
+    imgSrc: '/static/images/portfolio-images/PortfolioCard-UPMC-HP.png',
+    href: 'https://www.upmchealthplan.com/individuals/',
+  },
+  {
     title: 'Win Waste Innovation',
-    description: `Marketing & Portal Site Redesign built meticulously with Svelte client-side hydration and thoughtful UI considerations, such as the GSAP Animation Library and MirageJS mock-data services.`,
+    description: `Multi-Phase Marketing Site & Customer Portal Site built meticulously with Svelte client-side hydration and thoughtful UI considerations, such as the GSAP Animation Library and MirageJS mock-data services.`,
     imgSrc: '/static/images/portfolio-images/win-waste-thumbnail.png',
     href: 'https://www.win-waste.com/',
   },
